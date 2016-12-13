eea0d9ee7ab1ae2390f15b9dc28390b518b2d048
Yannis Smaragdakis <yannis@smaragd.org>
2016-12-13 17:33:24 +0200

    bug fix

--- src/soot/dexpler/DexField.java
+++ src/soot/dexpler/DexField.java
@@ -97,7 +97,8 @@ public class DexField {
         String name = f.getName();
         Type type = DexType.toSoot(f.getType());
         int flags = f.getAccessFlags();
-        String initialValue = f.getInitialValue().toString();
+        EncodedValue e = f.getInitialValue();
+        String initialValue = ((e == null) ? null : e.toString());
         SootField sf = new SootField(name, type, flags, initialValue);
         if (Modifier.isFinal(flags))
             DexField.addConstantTag(sf, f);
